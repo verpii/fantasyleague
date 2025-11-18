9d7a8e40c9e0f55d456988ac8c03757a2ab5d3f6
verpii <verpeleti.nandor@gmail.com>
2025-11-18 16:43:08 +0100

    Core Models

---
+++ fantasyleague/M/Player.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fantasyleague.M
+{
+    public enum PlayerRole
+    {
+        Top,
+        Jungle,
+        Mid,
+        Adc,
+        Support
+
+    }
+    public partial class Player : ObservableObject
+    {
+
+        [ObservableProperty]
+        string irlName;
+
+        [ObservableProperty]
+        string ign;
+
+        [ObservableProperty]
+        PlayerRole role;
+
+        [ObservableProperty]
+        int price;
+
+        [ObservableProperty]
+        string team;
+
+    }
+}
---
+++ fantasyleague/M/User.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fantasyleague.M
+{
+    public partial class User : ObservableObject
+    {
+        [ObservableProperty]
+        string username;
+        [ObservableProperty]
+        string userid;
+        [ObservableProperty]
+        int budget;
+        [ObservableProperty]
+        List<Player> roster = new List<Player>();
+    }
+}
